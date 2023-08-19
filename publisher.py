212bff3ebbf4b9bdfbeb7759a9ce3d592f419764
IanChaves <45765511+IanChaves@users.noreply.github.com>
2023-08-19 17:26:15 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,41 @@
-import zmq, time
-from constPS import * #-
+import zmq
+import threading
 
 context = zmq.Context()
-s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
-s.bind(p)                          # bind socket to the address
-while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+
+# Individual messaging (RPC)
+rpc_socket = context.socket(zmq.REQ)
+rpc_socket.connect("tcp://localhost:5555")
+
+# Topic-based messaging (Pub-Sub)
+sub_socket = context.socket(zmq.SUB)
+sub_socket.connect("tcp://localhost:5556")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "")
+
+def individual_sender():
+    while True:
+        recipient = input("Enter recipient username: ")
+        content = input("Enter message: ")
+        message = {"recipient": recipient, "content": content}
+        rpc_socket.send_json(message)
+        response = rpc_socket.recv_string()
+        print(response)
+
+def topic_sender():
+    while True:
+        content = input("Enter message for topic: ")
+        message = f"TOPIC {content}"
+        sub_socket.send_string(message)
+
+def topic_receiver():
+    while True:
+        message = sub_socket.recv_string()
+        print(f"Topic message received: {message}")
+
+individual_thread = threading.Thread(target=individual_sender)
+topic_sender_thread = threading.Thread(target=topic_sender)
+topic_receiver_thread = threading.Thread(target=topic_receiver)
+
+individual_thread.start()
+topic_sender_thread.start()
+topic_receiver_thread.start()
